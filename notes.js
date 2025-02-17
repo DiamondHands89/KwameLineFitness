9859f8eb91c5afece88f905136481e9f8e0ab504
DiamondHands89 <kwamemullins@outlook.com>
2025-02-17 12:47:54 -0500

    Tab List

---
+++ notes.js
@@ -0,0 +1,11 @@
+// Kw: Functional Purposeful
+
+// Aid: Spacing Sizing/ Tour Guide Experience
+
+//Must: Navigation Menu /Accessibility
+
+// Ow: Simple Elegant/ Attention Based Design[Component Based -sideNav]
+
+// Theme Based
+
+// Sw: Auto Adjusting/ Snap in
